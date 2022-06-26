a24866e6164ee28db6668425799f3fa4cbbecf39
kosuke55 <kosuke.tnp@gmail.com>
2022-06-26 19:37:33 +0900

    Clean up

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ from pathplanning import PathPlanning
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--x_start', type=int, default=75, help='X of start')
-    parser.add_argument('--y_start', type=int, default=45, help='Y of start')
-    parser.add_argument('--psi_start', type=int, default=-30, help='psi of start')
+    parser.add_argument('--y_start', type=int, default=40, help='Y of start')
+    parser.add_argument('--psi_start', type=int, default=-20, help='psi of start')
     parser.add_argument('--x_end', type=int, default=90, help='X of end')
     parser.add_argument('--y_end', type=int, default=80, help='Y of end')
-    parser.add_argument('--parking_length', '-p', type=int, default=20, help='parking_length [m]')
+    parser.add_argument('--parking_length', '-p', type=int, default=12, help='parking_length [m]')
     parser.add_argument('--parking_margin', '-m', type=int, default=1, help='parking_margin [m]')
     parser.add_argument('--last_backward_length', '-b', type=int, default=0, help='parking_margin [m]')
     parser.add_argument('--use_control', '-c', action='store_true', help='use control')
@@ -57,11 +57,6 @@ if __name__ == '__main__':
     env = Environment(
         obs,
         my_car,
-        # car_length * 10,
-        # car_width * 10,
-        # wheel_length * 10,
-        # wheel_width * 10,
-        # wheel_positions * 10,
         args.parking_margin
     )
 
@@ -74,36 +69,21 @@ if __name__ == '__main__':
     key = cv2.waitKey()
 
     path_planner = PathPlanning(obs, my_car, parking1)
-    print("start end: ", start,end)
-    path, stear = path_planner.plan_path(
+    path, steer = path_planner.plan_path(
         start[0] - my_car.a / 2 * np.cos(np.deg2rad(args.psi_start)),
         start[1] - my_car.a / 2 * np.sin(np.deg2rad(args.psi_start)),
         np.deg2rad(args.psi_start),
         end[0], end[1],
         args.last_backward_length)
-    print(len(path), len(stear))
     env.draw_footprint(path)
     env.draw_path(path)
 
     x_F = parking1.cars[1][0] - my_car.car_length / 2
-    # env.plot([[int(end[0]), int(end[1])]])
-    # env.plot([[int(end[1]), int(end[0])]])
-    # env.plot([[int(x_F), int(end[1])]])
     C_l = np.array(end + [0, path_planner.R_Elmin])
-    print(C_l)
-    # env.plot([C_l])
-    # env.plot([path_planner.B])
-    # env.plot([path_planner.F1])
-    # env.plot([path_planner.Cr])
-    # env.plot([path_planner.Cl])
-    # env.plot([path_planner.G2])
-    # env.plot([[int(end[1]), int(x_F)]])
 
     res = env.render(my_car.x, my_car.y, my_car.psi, 0)
     cv2.imshow('environment', res)
     key = cv2.waitKey()
-    # vehicle_center_path = path_planner.get_vehicle_center_path(path)
-    # for i, point in enumerate(vehicle_center_path):
     for i in range(len(path)):
         if args.use_control:
             vehicle_center_path = path_planner.get_vehicle_center_path(path)
@@ -112,8 +92,8 @@ if __name__ == '__main__':
             my_car.update_state(my_car.move(acc,  delta))
             x, y, psi = my_car.x, my_car.y, my_car.psi
         else:
-            delta = stear[i]
-            if delta != stear[i - 1] and i > 0:
+            delta = steer[i]
+            if delta != steer[i - 1] and i > 0:
                 cv2.imshow('environment', res)
                 key = cv2.waitKey(1)
                 sleep(0.5)
@@ -126,20 +106,16 @@ if __name__ == '__main__':
             y = point[1]
             if i < len(path) - 1:
                 v = path[i + 1] - point
-                # v = point - path[i + 1]
                 if np.any(v==0):
                     continue
-                # psi = np.arctan2(v[1], v[0])
                 psi = np.arctan(v[1] / v[0])
             x += my_car.a / 2 * np.cos(psi)
             y += my_car.a / 2 * np.sin(psi)
-                # sleep(0.3)
         res = env.render(x, y, psi, delta)
-        # sleep(0.1)
         is_collision = env.check_collision(x, y, psi)
 
-        # if is_collision:
-        #     break
+        if is_collision:
+            break
         cv2.imshow('environment', res)
         key = cv2.waitKey(1)
         if key == ord('s'):
--- pathplanning.py
+++ pathplanning.py
@@ -10,7 +10,6 @@ class PathPlanning:
         self.parking = parking
         self.R_Elmin = self.car.a / np.tan(self.car.steer_max)
         self.R_Ermin = self.R_Elmin
-        print("self.R_Elmin: ", self.R_Elmin)
         self.R_Bl_min = np.linalg.norm([self.R_Elmin + self.car.b + self.car.d_r,
                                         self.car.a + self.car.d_front], ord=2)
 
@@ -19,66 +18,41 @@ class PathPlanning:
             [self.R_Ermin - self.car.b - self.car.d_r, self.car.d_rear], ord=2)
         self.R_Jr_min = np.linalg.norm(
             [self.R_Ermin + self.car.b + self.car.d_l, self.car.d_rear], ord=2)
-        print("self.R_Bl_min: ", self.R_Bl_min)
-        # self.L_min = self.car.d_rear + np.sqrt(self.R_Bl_min**2 - (
-        # self.car.a / np.tan(self.car.steer_max) - self.car.b -
-        # self.car.d_l)**2)
         self.L_min = self.car.d_rear + np.sqrt(self.R_Bl_min**2 - (
             self.R_Elmin - self.car.b - self.car.d_l)**2)
-        print("L_min:{} ,+margin:{}".format(self.L_min,
-              self.L_min + self.parking.parking_margin))
-
-        # self.margin = 5
-        # sacale obstacles from env margin to pathplanning margin
-        # obstacles = obstacles + np.array([self.margin, self.margin])
-
-        # self.obs = np.concatenate([np.array([[0, i] for i in range(100 + self.margin)]),
-        #                           np.array([[100 + 2 * self.margin, i] for i in range(100 + 2 * self.margin)]),
-        #                           np.array([[i, 0] for i in range(100 + self.margin)]),
-        #                           np.array([[i, 100 + 2 * self.margin] for i in range(100 + 2 * self.margin)]),
-        #                           obstacles])
-
-        # self.ox = [int(item) for item in self.obs[:, 0]]
-        # self.oy = [int(item) for item in self.obs[:, 1]]
 
     def plan_path(self, sx, sy, sphi, gx, gy,
                   last_backward_length
                   ):
         if self.parking.parking_length > self.L_min + \
                 last_backward_length + self.parking.parking_margin * 2:
-            path, stear = self.plan_path_one_traial(
+            path, steer = self.plan_path_one_traial(
                 sx, sy, sphi, gx, gy, last_backward_length)
         else:
-            path, stear = self.plan_path_several_traial(
+            path, steer = self.plan_path_several_traial(
                 sx, sy, sphi, gx, gy, last_backward_length)
-        return path, stear
+        return path, steer
 
     def plan_path_one_traial(self, sx, sy, sphi, gx, gy,
                              last_backward_length, Cl=None, psi_last=0
                              ):
-        # path = np.array([np.linspace(sx, gx, 50), np.linspace(sy, gy, 50)])
         if Cl is None:
             Cl = np.array([gx, gy + self.R_Elmin])
         d_Cl_Einit = np.linalg.norm([sx, sy] - Cl, ord=2)
-        print("d_Cl_Einit: ", d_Cl_Einit)
         alpha = np.pi / 2 + sphi + np.arcsin((Cl[1] - sy) / d_Cl_Einit)
-        print("alpha: ", alpha)
         R_Einit_r = (d_Cl_Einit**2 - self.R_Elmin**2) \
             / (2 * (self.R_Elmin + d_Cl_Einit * np.cos(alpha)))
-        print("R_Einit_r: ", R_Einit_r)
         delta_r = np.arctan(self.car.a / R_Einit_r)
         Cr = np.array([sx + R_Einit_r * np.sin(sphi),
                        sy - R_Einit_r * np.cos(sphi)])
-        print("Cr: ", Cr)
 
         beta = np.arccos(
             (R_Einit_r**2 + (R_Einit_r + self.R_Elmin)**2 - d_Cl_Einit**2)
             / (2 * R_Einit_r * (R_Einit_r + self.R_Elmin)))
 
         point_interval = 0.25
-        # point_interval = 1.0
         path = [[sx, sy]]
-        stear = [0]
+        steer = [0]
 
         # turn right
         theta = np.pi / 2 + sphi
@@ -88,50 +62,35 @@ class PathPlanning:
             distance = np.linalg.norm([path[-1] - p_current])
             if distance > point_interval:
                 path.append(p_current)
-                stear.append(-np.arctan(self.car.a / R_Einit_r))
+                steer.append(-np.arctan(self.car.a / R_Einit_r))
             theta += 0.01
 
         # turn left
         theta = beta + sphi - np.pi / 2
         theta_last = -np.pi / 2 + psi_last
         while theta > theta_last:
-            # t = -np.pi / 2 + theta
             p_current = Cl + self.R_Elmin * \
                 np.array([np.cos(theta), np.sin(theta)])
             distance = np.linalg.norm([path[-1] - p_current])
             if distance > point_interval:
                 path.append(p_current)
-                stear.append(self.car.steer_max)
+                steer.append(self.car.steer_max)
             theta -= 0.01
-        # path.append(p_current)
-        # p_current = Cl + self.R_Elmin * \
-        #     np.array([np.cos(-np.pi / 2), np.sin(-np.pi / 2)])
-        # path.append(p_current)
 
-        # back straight
-        # l = point_interval
-        # while l < last_backward_length:
-        #     p_current = p_current + [-point_interval, 0]
-        #     path.append(p_current)
-        #     l += point_interval
-        # path.append([gx , gy])
-
-        return np.array(path), stear
+        return np.array(path), steer
 
     def plan_path_several_traial(self, sx, sy, sphi, gx, gy,
                                  last_backward_length
                                  ):
-        # print("too narrow !!")
         Cl = np.array([gx, gy + self.R_Elmin])
-        x_F = self.parking.cars[1][0] - self.car.car_length / 2 - self.parking.parking_margin
+        x_F = self.parking.cars[1][0] - \
+            self.car.car_length / 2 - self.parking.parking_margin
         d_Cl_F = x_F - Cl[0]
 
         point_interval = 0.1
         path = [[gx, gy]]
-        stear = [0]
+        steer = [0]
         psi = 0  # goal psi is 0
-        # theta = -np.pi / 2
-        print("Cl: ", Cl)
         while d_Cl_F < self.R_Bl_min + last_backward_length:
             B = np.array([gx + self.car.a + self.car.d_front,
                           gy - self.car.b - self.car.d_r])
@@ -144,76 +103,57 @@ class PathPlanning:
             # turn left
             theta = -np.pi / 2 + psi
             while theta < -np.pi / 2 + psi + theta_l:
-                # while psi < theta_l:
-                # print("theta, -np.pi / 2 + theta_l: " , theta , -np.pi / 2 + theta_l)
-                # self.al_kashi(Cl, B, goal - [0, self.car.b / 2 +
-                # self.car.d_r]):
                 p_current = Cl + self.R_Elmin * \
                     np.array([np.cos(theta), np.sin(theta)])
                 distance = np.linalg.norm([path[-1] - p_current])
                 if distance > point_interval:
                     path.append(p_current)
-                    stear.append(self.car.steer_max)
+                    steer.append(self.car.steer_max)
                 theta += 0.01
-                # psi += 0.01
             psi = theta + np.pi / 2
-            # psi = theta_l
 
-            # theta = np.pi / 2 + psi
             Cr = np.array([gx, gy - self.R_Ermin])
             Cr = self.transform(Cr, Cl, theta_l)
             self.Cr = Cr
             J = np.array([gx - self.car.d_rear,
                           gy + self.car.b + self.car.d_l])
             J = self.transform(J, Cl, theta_l)
-            x_G = self.parking.cars[0][0] + self.car.car_length / 2 + self.parking.parking_margin
+            x_G = self.parking.cars[0][0] + \
+                self.car.car_length / 2 + self.parking.parking_margin
             y_G2 = Cr[1] + np.sqrt(self.R_Jr_min**2 - (x_G - Cr[0])**2)
 
             G2 = np.array([x_G, y_G2])
             self.G2 = G2
             theta_r = self.al_kashi(Cr, J, G2)
-            print("theta_r: ", G2, theta_r)
 
-            # theta = np.pi / 2 + theta_l
-            # theta += np.pi
-            # turn right
             theta = np.pi / 2 + psi
             while theta < np.pi / 2 + psi + theta_r:
-                # print("theta, -np.pi / 2 + theta_l: " , theta , -np.pi / 2 + theta_l)
-                # self.al_kashi(Cl, B, goal - [0, self.car.b / 2 +
-                # self.car.d_r]):
                 p_current = Cr + self.R_Ermin * \
                     np.array([np.cos(theta), np.sin(theta)])
                 distance = np.linalg.norm([path[-1] - p_current])
                 if distance > point_interval:
                     path.append(p_current)
-                    stear.append(-self.car.steer_max)
+                    steer.append(-self.car.steer_max)
                 theta += 0.01
-            # path.append(p_current)
             psi = theta - np.pi / 2
 
             Cl = self.transform(Cl, Cr, theta_r)
             self.Cl = Cl
             d_Cl_F = x_F - Cl[0]
-        print("Cl: ", Cl)
-        retrieve_path, retrieve_stear = self.plan_path_one_traial(
+        retrieve_path, retrieve_steer = self.plan_path_one_traial(
             sx, sy, sphi, gx, gy, last_backward_length, Cl, psi)
 
         path = np.vstack((retrieve_path, path[::-1]))
-        stear = np.hstack((retrieve_stear, stear[::-1]))
-        # path=retrieve_path
+        steer = np.hstack((retrieve_steer, steer[::-1]))
 
-        # return np.array(path[::-1])
-        return path, stear
+        return path, steer
 
     def al_kashi(self, A, B, C):
         "calc alpha (the angle of A)"
         a = np.linalg.norm(B - C, ord=2)
         b = np.linalg.norm(A - C, ord=2)
         c = np.linalg.norm(A - B, ord=2)
-        print("a,b,c", a, b, c)
         alpha = np.arccos((b**2 + c**2 - a**2) / (2 * b * c))
-        print(alpha)
         return alpha
 
     def transform(self, p, C, theta):
